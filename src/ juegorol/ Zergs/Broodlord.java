f67eb5408847c7f16e8ec4c21a777bd52351ec32
ithalia <39180674+ithalia@users.noreply.github.com>
2018-05-14 02:55:22 -0500

    Create Broodlord.java

---
+++ src/ juegorol/ Zergs/Broodlord.java
@@ -0,0 +1,18 @@
+paquete  juegorol.Zerg ;
+  
+  Broodlord de clase púiblica extiende Zerg {
+       broodlord público () {
+       esta  . size = (float)  2 ;
+       esta . carga = 0 ;
+       esta . armadura  = 1    ;
+       esta . salud =  225 ;
+       esta . ataque = 0; 
+       esta . enfriamiento =  1.79 ;
+       esta . rango = 10 ;
+       esta . vista = 9;
+  }
+  pública Broodlord ( float tamaño, flotar de carga, int armadura, int salud, int ataqueT, int ataqueA, 
+               tiempo de reutilización flotante , int rango, int vista  )
+         {
+         }
+  }
